3a6f82a32790f4a8f97a03180d225efedaba2b85
Reid Watson <j.watsonreid@gmail.com>
2020-09-13 20:14:45 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -14,13 +14,13 @@ def apply_coupons(cart, coupons)
       if cart_item_with_coupon
        cart_item_with_coupon[:count] += coupons[counter][:num]
        cart_item[:count] -= coupon[counter][:num]
-     else
-       cart_item_with_coupon = {
-         :item => couponed_item_name,
-         :price => coupons[counter][:cost] / coupons[counter][:num],
-         :count => coupons[counter][:num],
-         :clearance => cart_item[:clearance]
-      }
+      else
+        cart_item_with_coupon = {
+          :item => couponed_item_name,
+          :price => coupons[counter][:cost] / coupons[counter][:num],
+          :count => coupons[counter][:num],
+          :clearance => cart_item[:clearance]
+        }
         cart << cart_item_with_coupon
         cart_item[:count] -= coupons[counter][:num]
       end
