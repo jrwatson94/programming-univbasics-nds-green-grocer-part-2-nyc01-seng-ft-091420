779ef8944d7aa05c592579e55aa331f4985173a3
Reid Watson <j.watsonreid@gmail.com>
2020-09-13 19:59:42 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -5,30 +5,7 @@ def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
-  coupons.each do |index|
-   cart_item = find_item_by_name_in_collection(index[:item],cart)
-   couponed_item_name = "#{index[:item]} W/ COUPON"
-   cart_item_with_coupon = find_item_by_name_in_collection(couponed_item_name,cart)
-   
-    if cart_item && cart_item[:count] >= index[:num]
-      if cart_item_with_coupon
-        cart_item_with_coupon[:count] += index[:num]
-        cart_item[:count] -= index[:num]
-      else
-        cart_item_with_coupon = {
-          :item => couponed_item_name,
-          :price => index[:cost] / index[:num],
-          :count => index[:num],
-          :clearance => cart_item[:clearance]
-        }
-        cart << cart_item_with_coupon
-        cart_item[:count] -= index[:num]
-     end
-     binding.pry
-    end
-  end
-  cart
-end
+  
     
 
 
